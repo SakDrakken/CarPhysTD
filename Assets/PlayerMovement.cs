86dc9973d9c5cf6c63b14316c646b6700680125a
SakDrakken <adamkrasa@seznam.cz>
2019-02-05 22:28:55 +0100

    testing out stuff

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float hor = .0f;
+    [SerializeField]
+    private float ver = .0f;
+
+    float mult = 1000f;
+
+    private Rigidbody player;
+
+    void Start () {
+        player = GetComponentInChildren<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(ver * mult, 0, -hor * mult);
+        player.AddTorque(force);
+	}
+}
---
+++ Assets/PlayerObjectFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObjectFollow : MonoBehaviour {
+
+    [SerializeField]
+    Transform player;
+
+	void LateUpdate () {
+        transform.position = player.transform.position; 
+	}
+}
