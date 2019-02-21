1c85c590a49a796ba4f4f21d3ff6d06323055ba7
SakDrakken <adamkrasa@seznam.cz>
2019-02-21 20:08:29 +0100

    progress day x

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,19 +11,53 @@ public class PlayerMovement : MonoBehaviour {
 
     float mult = 1000f;
 
-    private Rigidbody player;
+    private Rigidbody FLWheel;
+    private Rigidbody FRWheel;
+    private Rigidbody RLWheel;
+    private Rigidbody RRWheel;
+
+    private Transform FrontAxis;
+    private Transform RearAxis;
+
+    [SerializeField]
+    private bool isAWD = false;
+    [SerializeField]
+    private bool isFWD = true;
+    [SerializeField]
+    private bool isRWD = false;
+
 
     void Start () {
-        player = GetComponentInChildren<Rigidbody>();
-	}
+        FrontAxis = transform.Find("FrontAxis").GetComponent<Transform>();
+        RearAxis = transform.Find("RearAxis").GetComponent<Transform>();
+
+        FLWheel = FrontAxis.Find("LFWheel").GetComponent<Rigidbody>();
+        FRWheel = FrontAxis.Find("RFWheel").GetComponent<Rigidbody>();
+        RLWheel = RearAxis.Find("LRWheel").GetComponent<Rigidbody>();
+        RRWheel = RearAxis.Find("RRWheel").GetComponent<Rigidbody>();
+    }
 	
 	void FixedUpdate () {
         hor = Input.GetAxis("Horizontal");
         ver = Input.GetAxis("Vertical");
 
-        Vector3 force = new Vector3(ver * mult, 0, ver * mult);
-        Vector3 turnforce = new Vector3(0, hor * mult, 0);
-        player.AddRelativeTorque(force);
-        player.AddTorque(turnforce);
+        Vector3 force = new Vector3(ver * mult, 0, 0);
+
+        if(isAWD)
+        {
+            isFWD = isRWD = true;
+        }
+
+        if(isFWD)
+        {
+            FRWheel.AddTorque(force);
+            FLWheel.AddTorque(force);
+        }
+        if(isRWD)
+        {
+            RRWheel.AddTorque(force);
+            RLWheel.AddTorque(force);
+        }
+        
     }
 }
